ba346821ef9b6ec4d2548f047e807eb02a5f53b7
primeboogie <primemarkboogie@gmail.com>
2024-07-10 13:55:54 -0100

    Compeletd

--- server/app.py
+++ server/app.py
@@ -8,19 +8,105 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    
+    def post(self):
+        recived_request = request.get_json()
+
+        username = recived_request.get('username')
+        password = recived_request.get('password')
+        image_url = recived_request.get('image_url')
+        bio = recived_request.get('bio')
+
+        user = User(
+            username=username,
+            image_url=image_url,
+            bio=bio
+        )
+
+        user.password_hash = password
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            session['user_id'] = user.id
+
+            return user.to_dict(), 201
+
+        except IntegrityError:
+
+            return {'error': '422'}, 422
+
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session['user_id']
+
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            return user.to_dict(), 200
+        else:
+            return {}, 401
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        recived_request = request.get_json()
+
+        username = recived_request.get('username')
+        password = recived_request.get('password')
+        
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+
+    def delete(self):
+        if session['user_id']:
+            session['user_id'] = None
+            return {'message': '204: No Content'}, 204
+        return {}, 401
 
 class RecipeIndex(Resource):
-    pass
+    
+    def get(self):
+        if session['user_id']:
+            user = User.query.filter_by(id = session['user_id']).first()
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        return {}, 401
+    
+    def post(self):
+        if session['user_id']:
+            recived_request = request.get_json()
+
+            title = recived_request['title']
+            instructions = recived_request['instructions']
+            minutes_to_complete = recived_request['minutes_to_complete']
+
+            try:
+
+                recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id'],
+                )
+
+                db.session.add(recipe)
+                db.session.commit()
+
+                return recipe.to_dict(), 201
+
+            except IntegrityError:
+
+                return {'error': '422'}, 422
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
